a3e12ac5992f403f5c062824719fdebdc34baaf3
Zogoo <chtsogbadrakh@gmail.com>
2020-03-12 01:11:01 +0900

    Add required data

---
+++ App.vue
@@ -0,0 +1,43 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import HomeTab from "./src/tabs/HomeTab";
+import MenuTab from "./src/tabs/MenuTab";
+import HistoryTab from "./src/tabs/HistoryTab";
+
+// An iOS style bottom tab-bar navigator
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Menu: MenuTab,
+    History: HistoryTab
+  }
+);
+
+// An Android style top tab-bar navigator
+const MaterialTopTabNavigator = createMaterialTopTabNavigator(
+  {
+    Menu: MenuTab,
+    History: HistoryTab
+  }
+);
+
+// The navigators can be used as regular screens in any other navigator
+const StackNavigator = createStackNavigator(
+  {
+    Home: HomeTab,
+    IOSTabs: BottomTabNavigator,
+    AndroidTabs: MaterialTopTabNavigator
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+</script>
---
+++ src/tabs/HomeTab.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="container">
+    <text class="heading">Home</text>
+    <text class="text">This is the Home screen</text>
+    <text class="text">Use the swipe gesture to open the drawer navigator</text>
+    <button title="View fuck" @press="goToIOSTabNavigator"></button>
+    <button title="View aaa" @press="goToAndroidTabNavigator"></button>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: { type: Object }
+  },
+  methods: {
+    goToIOSTabNavigator() {
+      this.navigation.navigate("MenuTab")
+    },
+    goToAndroidTabNavigator() {
+      this.navigation.navigate("HistoryTab")
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  padding: 50px;
+}
+.heading {
+  font-size: 30px;
+  font-weight: bold;
+  color: darkolivegreen;
+  margin: 20px;
+}
+.text {
+  text-align: center;
+  margin: 10px;
+}
+</style>
